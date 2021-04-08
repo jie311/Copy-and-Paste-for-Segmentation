dc41e6e8420a1a741046b20a35cf383ebdfbc216
Wei Chih <pasture800222@gmail.com>
2021-04-07 20:43:57 -0400

    Worked with albumentations now

--- SemanticCopyandPaste.py
+++ SemanticCopyandPaste.py
@@ -7,9 +7,10 @@ import matplotlib.pyplot as plt
 
 
 
-class SemanticCopyandPaste():
+class SemanticCopyandPaste(A.DualTransform):
     
-    def __init__(self, nClass, path2rgb, path2mask):
+    def __init__(self, nClass, path2rgb, path2mask, always_apply=False, p=0.5):
+        super().__init__(always_apply=always_apply, p=p)
         self.nClass     = nClass
         self.rgb_base   = path2rgb
         self.mask_base  = path2mask
@@ -17,15 +18,20 @@ class SemanticCopyandPaste():
         self.masks      = os.listdir(path2mask)
         self.nImages    = len(self.rgbs)
         self.threshold  = 30
-        assert len(self.rgbs) == len(self.masks), "rgb path's file count != mask path's file count"
+        self.targetClass= 0
+        self.c_image    = None # candidate image
+        self.c_mask     = None # candidate mask
+        self.found      = False
         
+        assert len(self.rgbs) == len(self.masks), "rgb path's file count != mask path's file count"
+        assert self.nClass > 0, "Incorrect class number"
+
         
     
-    def apply(self, image, mask):
+    def apply(self, image, **params):
         '''
-            Input:
+            Args:
                 image: 3-channel RGB images
-                mask : 3-channel mask with pixel values of class index. Ex: 0 = background, 1 = class#1, etc
 
            This function will first randomly generate a class that being copied (Exclude 0, which is the background class). Then randomly picks a mask via provided path, and search whether it contains the previously picked target class. Keep randomly picks a new mask until a match is found. Finally start doing copy and paste process.
 
@@ -36,29 +42,87 @@ class SemanticCopyandPaste():
                2. Mask shifting before pasting
                3.
         '''
-        targetClass = 0
-        while targetClass == 0:
-            targetClass = random.randrange(self.nClass)
         
-        print("Target Class = ", targetClass)
         
+        while self.targetClass == 0:
+            self.targetClass = random.randrange(self.nClass)
+                
         ret = True
-        
         while ret:
             candidate = random.randrange(self.nImages)
-            c_image   = cv2.imread(os.path.join(self.rgb_base, self.rgbs[candidate]))   
+            c_image   = cv2.imread(os.path.join(self.rgb_base, self.rgbs[candidate]))
+            c_image   = cv2.cvtColor(c_image, cv2.COLOR_BGR2RGB)
             c_mask    = cv2.imread(os.path.join(self.mask_base, self.masks[candidate]))
-            if self.target_class_in_image(c_mask, targetClass):
+            if self.target_class_in_image(c_mask, self.targetClass):
                 ret = False
-            
-#             print("rgb =", c_image.shape)
-#             print("mask =", c_mask.shape)
-#             input()
-        return self.copy_and_paste(c_image, c_mask, image, mask, self.nClass, targetClass)
+                self.found       = True
+                self.c_mask      = c_mask
+                self.c_image     = c_image
+                
+        
+        return self.copy_and_paste_image(self.c_image, self.c_mask, image, self.targetClass)
     
     
     
-    # Take 3-channel mask
+    def apply_to_mask(self, mask, **params):
+        assert self.found == True
+        assert self.c_mask is not None # copy_and_paste_image has to be ran first to pass this
+    
+        return self.copy_and_paste_mask(self.c_mask, mask, self.targetClass)
+    
+
+    
+    
+    # Augmentation will be added to rgb2 (extract content from rgb1)
+    # Mask1 is need to know where to extract pixels for color image copy and paste
+    def copy_and_paste_image(self, rgb1, mask1, rgb2, targetClassForAug):
+        assert rgb1 is not None
+        assert rgb2 is not None
+        assert mask1 is not None
+        assert mask1.shape[2] == 3 # We imread it without further process, so its a 3 channel
+        
+        tmp   = mask1[...,1] # All 3 channels have same content, we take 1 to process
+        masks = [(tmp == v) for v in range(self.nClass)] 
+        masks = np.stack(masks, axis=-1).astype('float') # mask.shape = (x,y,ClassNums)
+        self.c_mask = masks
+
+        for i in range(3): #r,g,b channels
+            rgb2[...,i] = rgb2[...,i] - rgb2[...,i]*masks[..., targetClassForAug] + rgb1[...,i] * masks[..., targetClassForAug]
+
+        return rgb2
+
+    
+    
+    
+    def copy_and_paste_mask(self, mask1, mask2, targetClassForAug):
+        '''
+            Args:
+                mask1 = randomly picked qualified mask from apply(), has shape = (x, y, nClasses)
+                mask2 = dataloader loaded mask, aug is added to mask2
+        '''
+        assert mask2.shape[2] == self.nClass # Processed by dataloader, so its a nClass channel
+    
+        mask2_1channel = np.argmax(mask2, axis=2)
+        
+#         mask1 = [(mask1[..., 0] == v) for v in range(self.nClass)]
+#         mask1 = np.stack(mask1, axis=-1).astype('float')
+        
+        newMask = mask2_1channel - mask2_1channel * mask1[..., targetClassForAug] + targetClassForAug * mask1[..., targetClassForAug] 
+        
+        masks = [(newMask == v) for v in range(self.nClass)] # mask.shape = (x,y,ClassNums)
+        masks = np.stack(masks, axis=-1).astype('float')
+           
+        # Reset
+        self.c_mask = None 
+        self.found == False
+        return masks
+    
+    
+    
+    
+    
+    
+    # We imread the mask, so it's a 3-channel mask (not one-hot encoded)
     def target_class_in_image(self, mask, targetClassIdx):
         m   = mask[..., 0]
         tmp = (m==targetClassIdx)
@@ -67,31 +131,13 @@ class SemanticCopyandPaste():
         else:
             return False
     
-    # Augmentation will be done on rgb2 (extract info from rgb1)
-    # Extract class mask from rgb1 & mask1 and paste it on rgb2 & mask2
-    def copy_and_paste(self, rgb1, mask1, rgb2, mask2, numClass, targetClassForAug):
-        assert numClass > 0, "Incorrect class number"
-        assert rgb1 is not None
-        assert rgb2 is not None
-        assert mask1 is not None
-        assert mask2 is not None
-        assert mask1.shape[2] == 3
-        assert mask2.shape[2] == 3
-        
-        tmp = mask1[...,1]
-        masks = [(tmp == v) for v in range(self.nClass)] # mask.shape = (x,y,ClassNums)
-        masks = np.stack(masks, axis=-1).astype('float')
-        
-    #     assert np.sum(masks[...,targetClassForAug]) != 0, "Selected target class for augmentation has no content (all zeros)"
+    
+    def apply_to_bbox(self, bbox, **params):
+        return bbox
 
-        
-        newMask = mask2[...,1] - mask2[...,1] * masks[..., targetClassForAug] + targetClassForAug * masks[..., targetClassForAug] #new mask
+    def apply_to_keypoint(self, keypoint, **params):
+        return keypoint
 
-        for i in range(3): #r,g,b channels
-            rgb2[...,i] = rgb2[...,i] - rgb2[...,i]*masks[..., targetClassForAug] + rgb1[...,i] * masks[..., targetClassForAug]
-            mask2[...,i]= newMask
-        
-        return rgb2, mask2
-
-
-    __call__ = apply
+    def get_transform_init_args_names(self):
+        return ("image", "mask")
+    
